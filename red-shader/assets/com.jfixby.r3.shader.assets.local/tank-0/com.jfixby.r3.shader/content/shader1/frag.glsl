ca23ff6832980773b0115066591bc41a9c235c97
J Fixby <github@jfixby.com>
2017-01-16 17:22:34 +0100

    ...

--- red-shader/assets/com.jfixby.r3.shader.assets.local/tank-0/com.jfixby.r3.shader/content/shader1/frag.glsl
+++ red-shader/assets/com.jfixby.r3.shader.assets.local/tank-0/com.jfixby.r3.shader/content/shader1/frag.glsl
@@ -33,16 +33,22 @@ float distance(vec2 x, vec2 y) {
 	vec2 diff = y - x;
 	return sqrt(dot(diff, diff));
 }
+float sinS(float min, float max, float t) {
+	return ((sin(t) + 1.0) / 2.0) * (max - min) + min;
+
+}
 float F(float x, float y, float t) {
 	vec2 resolution = vec2(screen_width, screen_height);
 	vec2 center = vec2(screen_width / 2.0, screen_height / 2.0);
 	vec2 pos = vec2(x, y) - center;
+	float minAlpha = 0.4;
+	float timeM = 0.8;
 	float radius = min(center.x, center.y) * 0.9
-			* (0.25 + (1.0 - 0.25) * ((sin(t / 4.0) + 1.0) / 2.0));
-
+			* (minAlpha + (1.0 - minAlpha) * ((sin(t * timeM) + 1.0) / 2.0));
 
 	float dist = distance(pos, vec2(0.0, 0.0));
-	if (dist < radius && dist > radius * 0.8) {
+	if (dist < radius
+			&& dist > radius * 0.6 * sinS(0.4, 1.0, t * 2.0 * timeM)) {
 		return 1.0;
 	}
 	return 0.0;
@@ -59,16 +65,23 @@ vec4 finalColor(vec2 vTexCoord, float t, float p) {
 	color.g = vTexCoord.x;
 	color.b = vTexCoord.y;
 
-	int w = 0;
-	int h = 0;
+	int w = 1;
+	int h = 1;
 	float counter = 0.0;
 	float alpha = 0.0;
 	for (int i = -w; i <= w; i++) {
 		for (int j = -h; j <= h; j++) {
-			counter++;
+			float weight = 1.0;
+			if (i == 0) {
+				weight = weight + 2.0;
+			}
+			if (j == 0) {
+				weight = weight + 2.0;
+			}
 			float x = screen_width * vTexCoord.x + float(i);
 			float y = screen_height * vTexCoord.y + float(j);
-			alpha = alpha + F(x, y, t);
+			alpha = alpha + weight * F(x, y, t);
+			counter = counter + weight;
 		}
 	}
 	color.a = alpha / counter;
