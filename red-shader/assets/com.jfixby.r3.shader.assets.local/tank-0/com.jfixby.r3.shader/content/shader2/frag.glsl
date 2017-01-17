ca2d9ed76c0d81db93d663b06cb244dbeb8cfdb1
J Fixby <github@jfixby.com>
2017-01-17 19:19:12 +0100

    preloader

--- red-shader/assets/com.jfixby.r3.shader.assets.local/tank-0/com.jfixby.r3.shader/content/shader2/frag.glsl
+++ red-shader/assets/com.jfixby.r3.shader.assets.local/tank-0/com.jfixby.r3.shader/content/shader2/frag.glsl
@@ -33,26 +33,22 @@ float distance(vec2 x, vec2 y) {
 	vec2 diff = y - x;
 	return sqrt(dot(diff, diff));
 }
-
-float distanceMax(vec2 x, vec2 y) {
-	vec2 diff = y - x;
-	return max(abs(diff.x), abs(diff.y));
-}
 float sinS(float min, float max, float t) {
 	return ((sin(t) + 1.0) / 2.0) * (max - min) + min;
 
 }
-float F(float x, float y, float t, float p) {
+float F(float x, float y, float t) {
 	vec2 resolution = vec2(screen_width, screen_height);
-	vec2 center = vec2(screen_width * 0.5, screen_height * 0.7);
+	vec2 center = vec2(screen_width / 2.0, screen_height / 2.0);
 	vec2 pos = vec2(x, y) - center;
 	float minAlpha = 0.4;
 	float timeM = 0.8;
-	float radius = min(screen_width, screen_height) * 0.4;
-//			* (minAlpha + (1.0 - minAlpha) * ((sin(t * timeM) + 1.0) / 2.0));
-	pos.y = pos.y;
-	float dist = distanceMax(pos, vec2(0.0, 0.0));
-	if ((abs(pos.x) < radius) && (abs(pos.y) < radius * 0.01)) {
+	float radius = min(center.x, center.y) * 0.9
+			* (minAlpha + (1.0 - minAlpha) * ((sin(t * timeM) + 1.0) / 2.0));
+
+	float dist = distance(pos, vec2(0.0, 0.0));
+	if (dist < radius
+			&& dist > radius * 0.6 * sinS(0.4, 1.0, t * 2.0 * timeM)) {
 		return 1.0;
 	}
 	return 0.0;
@@ -65,13 +61,12 @@ vec4 finalColor(vec2 vTexCoord, float t, float p) {
 	vec4 color = vec4(1.0, 1.0, 1.0, 1.0);
 
 	color.a = 1.0;
-//	color.r = 0.05 + 0.9 * pow(cos(-time * 0.4 + vTexCoord.x), 2.0);
-	color.r = sinS(-0.10, 0.99, -time * 0.5 + vTexCoord.x * 4.0);
-	color.g = color.r * sinS(0.0, 1.0, time * 0.3);
-	color.b = 0.0;
+	color.r = sin(time) * sin(time) * 0.0 + 1.0;
+	color.g = vTexCoord.x;
+	color.b = vTexCoord.y;
 
-	int w = 0;
-	int h = 0;
+	int w = 1;
+	int h = 1;
 	float counter = 0.0;
 	float alpha = 0.0;
 	for (int i = -w; i <= w; i++) {
@@ -85,7 +80,7 @@ vec4 finalColor(vec2 vTexCoord, float t, float p) {
 			}
 			float x = screen_width * vTexCoord.x + float(i);
 			float y = screen_height * vTexCoord.y + float(j);
-			alpha = alpha + weight * F(x, y, t, p);
+			alpha = alpha + weight * F(x, y, t);
 			counter = counter + weight;
 		}
 	}
