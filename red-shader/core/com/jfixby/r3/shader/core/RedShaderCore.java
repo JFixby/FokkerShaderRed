dd2cde51312eccf419ab89ef36aec671974f7c38
J Fixby <github@jfixby.com>
2017-03-11 21:11:29 +0100

    ...

--- red-shader/core/com/jfixby/r3/shader/core/RedShaderCore.java
+++ red-shader/core/com/jfixby/r3/shader/core/RedShaderCore.java
@@ -1,17 +1,17 @@
 
 package com.jfixby.r3.shader.core;
 
-import com.jfixby.r3.api.logic.BusinessLogicComponent;
+import com.jfixby.r3.api.logic.GameStarter;
 import com.jfixby.r3.api.ui.UI;
 import com.jfixby.scarabei.api.assets.ID;
 import com.jfixby.scarabei.api.assets.Names;
 
-public class RedShaderCore implements BusinessLogicComponent {
+public class RedShaderCore implements GameStarter {
 
 	public static final ID unit_id = Names.newID("com.jfixby.r3.shader.ui.ShaderUI");
 
 	@Override
-	public void start () {
+	public void onGameStart () {
 
 		UI.loadUnit(unit_id);
 
